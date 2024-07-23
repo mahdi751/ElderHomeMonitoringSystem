25c79c52045518ed4768e15115b60b77694e5655
NinjaBoomlb <tkhatib321x@gmail.com>
2024-07-23 17:51:22 +0300

    Making SleepSessionModel

---
+++ ElderHomeMonitoringSystem/Models/SleepSession.cs
@@ -0,0 +1,10 @@
+﻿namespace ElderHomeMonitoringSystem.Models
+{
+    public class SleepSession
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; } 
+    }
+}
